049a43daabf430149ffa47d0a272c3c902443bcb
Daniil Girskiy <daniil.girskiy@mindera.com>
2022-12-09 11:33:53 +0000

    add custom tabbar with extensions

---
+++ Sources/MinderaPeople-iOS-DesignSystem/Components/Views/TabBarView.swift
@@ -0,0 +1,103 @@
+import SwiftUI
+
+public class TabBarController<SelectionValue>: ObservableObject where SelectionValue: Hashable {
+    @Binding public var selected: SelectionValue
+    @Published public var isVisible: Bool = true
+    
+    public init(selected: Binding<SelectionValue>) {
+        _selected = selected
+    }
+}
+
+
+public struct TabBarView<SelectionValue>: View where SelectionValue: Hashable {
+    private struct Tab {
+        var selection: SelectionValue
+        var icon: AnyView
+        var text: AnyView
+        var content: AnyView
+    }
+    
+    @Environment(\.safeAreaInsets) private var safeAreaInsets
+    @ObservedObject
+    private var controller: TabBarController<SelectionValue>
+    private var tabs: [Tab] = []
+    
+    private init(controller: TabBarController<SelectionValue>, tabs: [Tab]) {
+        self.controller = controller
+        self.tabs = tabs
+    }
+    
+    public init(selection: Binding<SelectionValue>) {
+        controller = .init(selected: selection)
+    }
+    
+    public var body: some View {
+        ZStack(alignment: .bottom) {
+            content
+                .frame(maxHeight: .infinity)
+                .environmentObject(controller)
+            bar
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .frame(maxHeight: .infinity)
+    }
+    
+    @ViewBuilder
+    private var bar: some View {
+        HStack(alignment: .top, spacing: 0) {
+            ForEach(Array(tabs.enumerated()), id: \.offset) { tab in
+                Spacer()
+                Button (action: {
+                    controller.selected = tab.element.selection
+                }) {
+                    VStack{
+                        tab.element.icon
+                        tab.element.text
+                            .font(.system(size: 10))
+                        
+                    }
+                }
+                .foregroundColor(controller.selected == tab.element.selection ? Color.indigo(._600) : Color.greyBlue(._500))
+            }
+            Spacer()
+        }
+        .padding(.top, 12)
+        .padding(.bottom, safeAreaInsets.bottom)
+        .background(Color.greyBrown(._000))
+        .offset(y: controller.isVisible ? 0 : 80)
+        .animation(.easeInOut, value: controller.isVisible)
+    }
+    
+    @ViewBuilder
+    private var content: some View {
+        ZStack {
+            ForEach(tabs, id: \.selection) { tab in
+                tab.content
+                    .disabled(tab.selection != controller.selected)
+                    .opacity(tab.selection == controller.selected ? 1 : 0)
+                    .animation(.easeInOut(duration: 0.2), value: controller.selected)
+            }
+        }
+    }
+    
+    public func tab<A: View, B: View, C: View>(_ selectionValue: SelectionValue,
+                                               content: () -> A,
+                                               icon: () -> B,
+                                               text: () -> C) -> TabBarView {
+        let tab = Tab(
+            selection: selectionValue,
+            icon: AnyView(icon()),
+            text: AnyView(text()),
+            content: AnyView(content())
+        )
+        var tabs = tabs
+        if let index = tabs.firstIndex(where: { tab in
+            tab.selection == selectionValue
+        }) {
+            tabs.remove(at: index)
+        }
+        tabs.append(tab)
+        return .init(controller: controller, tabs: tabs)
+    }
+}
---
+++ Sources/MinderaPeople-iOS-DesignSystem/Extensions/EnvironmentValues+Extension.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+extension UIApplication {
+    var keyWindow: UIWindow? {
+        connectedScenes
+            .compactMap {
+                $0 as? UIWindowScene
+            }
+            .flatMap {
+                $0.windows
+            }
+            .first {
+                $0.isKeyWindow
+            }
+    }
+}
+
+private struct SafeAreaInsetsKey: EnvironmentKey {
+    static var defaultValue: EdgeInsets {
+        UIApplication.shared.keyWindow?.safeAreaInsets.insets ?? EdgeInsets()
+    }
+}
+
+extension EnvironmentValues {
+    var safeAreaInsets: EdgeInsets {
+        self[SafeAreaInsetsKey.self]
+    }
+}
+
+private extension UIEdgeInsets {
+    var insets: EdgeInsets {
+        EdgeInsets(top: top, leading: left, bottom: bottom, trailing: right)
+    }
+}
