6dcd590571d9a3f8eec716a37a53abb51521b90e
Pedro Seruca <pedroseruca@gmail.com>
2022-12-20 16:13:51 +0000

    MPA-15: Rename module

--- Tests/MinderaDesignSystemTests/MinderaDesignSystemTests.swift
+++ Tests/MinderaDesignSystemTests/MinderaDesignSystemTests.swift
@@ -1,7 +1,7 @@
 import XCTest
-@testable import MinderaPeople_iOS_DesignSystem
+@testable import MinderaDesignSystem
 
-final class MinderaPeople_iOS_DesignSystemTests: XCTestCase {
+final class MinderaDesignSystemTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
